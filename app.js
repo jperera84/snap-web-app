13fb1dc5eac5b9c8f807b35616baa1494f687ea8
Jose R Perera Morales <jpereramor@gmail.com>
2023-09-06 21:18:07 -0400

    Updated snap config

--- app.js
+++ app.js
@@ -4,14 +4,14 @@
 
 async function getNodeAPI(e) {
     const response = await fetch("http://localhost:3000/");
-    const data = await response.json();
+    const data = await response.text();
     const nodeResponseElement = document.getElementById("nodeResponse");
     nodeResponseElement.textContent = data;
 }
 
 async function getPythonAPI(e) {
     const response = await fetch("http://localhost:5000/");
-    const data = await response.json();
+    const data = await response.text();
     const pythonResponseElement = document.getElementById("pythonResponse");
     pythonResponseElement.textContent = data;
 }
